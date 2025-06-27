b2ad784039d10cf416e64b3adb16f194c15300d2
Harshit Mohan <cip.2019.dev@gmail.com>
2025-06-27 20:42:40 +0530

    testing streamlit

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
 #Code to generate BI reports
+import streamlit as st
+st.title("Hello Harshit")
+
+st.write("👋 Hello, this is a sample text displayed using `st.write()`.")
+
+st.text("This is plain text using st.text().")
+
+st.markdown("**This is bold text using markdown!**")
+
+st.success("This is a success message.")
+st.info("This is an info message.")
+st.warning("This is a warning.")
+st.error("This is an error message.")
